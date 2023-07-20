94e4d6e8a266b090d161b39f6b68b8df05a7c254
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-07-20 11:36:12 -0300

    feat(site): add drawer itens

--- src/packages/site/ui/Drawer/Items.tsx
+++ src/packages/site/ui/Drawer/Items.tsx
@@ -1,13 +1,38 @@
-import { Description } from "@radix-ui/react-dialog";
 import { Button, Icon } from "@design-system/ui";
+import { styled } from "@/lib/stitches.config";
 
 export const Items = () => {
   return (
-    <Description>
-      <Button>
-        <Icon icon="search" />
-        Search
-      </Button>
-    </Description>
+    <Container>
+      <Item>
+        <Icon size={14} icon="search" />
+        <Text>Search</Text>
+      </Item>
+      <Item>
+        <Icon size={14} icon="clock" />
+        <Text>Updates</Text>
+      </Item>
+    </Container>
   );
 };
+
+const Item = styled(Button, {
+  alignItems: "center",
+  display: "flex",
+  gap: "$2",
+  justifyContent: "flex-start",
+  margin: "1px 0",
+  padding: "2px 10px",
+  width: "100%",
+});
+
+const Text = styled("p", {
+  color: "$primary200",
+  fontSize: 14,
+  lineHeight: "21px",
+});
+
+const Container = styled("section", {
+  borderBottom: "1px solid rgba(15, 15, 15, 0.1)",
+  padding: "0 $1 $2",
+});
